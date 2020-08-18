d56ac4bf6a461dd2f756b4d8540a26483d054ca0
osechi3 <holypoetwrk@gmail.com>
2020-08-18 20:54:39 +0300

    Adding style to WeatherCard

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="container">
-    <div id="header">
-      <h1>{{ currentWeather.location }}</h1>
+    <div>
+      <h1 id="header">{{ currentWeather.location }}</h1>
     </div>
     <div id="body">
-      <div>
-        <img :src="currentWeather.iconURL" alt="">
-        <p>{{ currentWeather.weatherDescription.main }}</p>
+        <div id="block-center">
+          <div class="group-icon">
+          <img :src="currentWeather.iconURL" alt="">
+          <p id="weather-description">{{ currentWeather.weatherDescription.main }}</p>
+        </div>
+        <div class="group-temperature">
+          <p class="temperature-number">{{ temperatureCelsius }}°C</p>
+          <div class="group-btn">
+            <button class="btns-temperature">°C</button>
+            <button class="btns-temperature">°F</button>
+          </div>
+        </div>
+        <p class="feels-like">Feels like: {{ currentWeather.feelsLike }}</p>
       </div>
-      <div>
-        <p>{{ temperatureCelsius }} {{ temperatureFahrenheit }}</p>
-        <button>C</button>
-        <button>F</button>
-      </div>
-      <p>Feels like: {{ currentWeather.feelsLike }}</p>
-      <div>
-        <p>Humidity: {{ currentWeather.humidity }}%</p>
-        <p>Wind: {{ currentWeather.windSpeed }} m/s</p>
+      <div id="block-bottom">
+        <div>
+          <p>Humidity: {{ currentWeather.humidity }}%</p>
+          <p>Wind: {{ currentWeather.windSpeed }} m/s</p>
+        </div>
       </div>
     </div>
   </div>
@@ -41,5 +47,93 @@ export default {
 }
 </script>
 <style scoped>
+  #container {
+    display: flex;
+    flex-direction: column;
 
+    width: 660px;
+    height: 330px;
+    margin: 65px auto 0 auto;
+
+    background-color: white;
+    opacity: .8;
+  }
+  #header {
+    margin: 0;
+    padding: 10px 0 10px 0;
+
+    text-align: center;
+    font-size: 32px;
+    color: white;
+
+    background-color: #2C8C99;
+  }
+  #body {
+    display: flex;
+    flex-direction: column;
+  }
+
+  #block-center {
+    display: flex;
+    justify-content: center;
+
+    margin-top: 25px;
+  }
+
+  .group-icon {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    margin-top: 10px;
+  }
+  #weather-description {
+    position: absolute;
+    top: 85px;
+    margin: 0;
+
+    font-size: 20px;
+  }
+
+  .group-temperature {
+    margin: 25px 0;
+  }
+  .temperature-number {
+    margin: 0;
+
+    text-align: center;
+    font-size: 70px;
+  }
+
+  .group-btn {
+    display: flex;
+    justify-content: center;
+  }
+  .btns-temperature {
+    margin: 0 5px;
+
+    font-size: 17px;
+
+    border: none;
+  }
+  .btns-temperature:hover {
+    background-color: #2C8C99;
+    color: white;
+  }
+
+  .feels-like {
+    align-self: flex-end;
+
+    margin: 0 5px 25px 5px;
+  }
+
+  #block-bottom {
+    display: flex;
+    justify-content: flex-end;
+
+    margin-right: 15px;
+
+    font-size: 18px;
+  }
 </style>
