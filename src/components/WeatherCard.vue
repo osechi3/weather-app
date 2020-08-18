c1e243652eb2cc69b96903c59850cacb338d0d1c
osechi3 <holypoetwrk@gmail.com>
2020-08-18 08:42:41 +0300

    Creating a basic card structure

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="container">
-
+    <div id="header">
+      <h1>California</h1>
+    </div>
+    <div id="body">
+      <div>
+        <img src="#" alt="">
+        <p>Description</p>
+      </div>
+      <div>
+        <p>Temperature</p>
+        <button>C</button>
+        <button>F</button>
+      </div>
+      <p>Feels like:</p>
+      <div>
+        <p>Humidity:</p>
+        <p>Wind:</p>
+      </div>
+      <p>Time:</p>
+    </div>
   </div>
 </template>
 <script>
