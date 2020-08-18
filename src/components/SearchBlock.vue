e97242b91bfd6b7dc69dbf143c9bbee3cc86d48c
osechi3 <holypoetwrk@gmail.com>
2020-08-18 19:44:51 +0300

    Adding styling to the search component

--- src/components/SearchBlock.vue
+++ src/components/SearchBlock.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="container">
-    <input v-model="city" type="text">
-    <button type="button" @click="$emit('get-weather-btn-clicked', city)">Get Weather</button>
+    <input
+      id="input-field"
+      v-model="city"
+      type="text">
+    <button
+      id="btn-search"
+      type="button"
+      @click="$emit('get-weather-btn-clicked', city)">Search
+    </button>
   </div>
 </template>
 <script>
@@ -14,5 +21,37 @@ export default {
 }
 </script>
 <style scoped>
+  #container {
+    display: flex;
+    justify-content: center;
 
+    width: 800px;
+    margin: 60px auto 0 auto;
+
+    background-color: white;
+    opacity: .8;
+  }
+  #input-field {
+    width: 670px;
+    padding-left: 10px;
+
+    font-size: 30px;
+
+    border: none;
+  }
+  #btn-search {
+    width: 130px;
+    height: 70px;
+
+    font-size: 24px;
+    color: white;
+    font-weight: bold;
+
+    border: none;
+    background: #25283D;
+  }
+  #btn-search:hover {
+    background-color: #E0E0E1;
+    color: black;
+  }
 </style>
